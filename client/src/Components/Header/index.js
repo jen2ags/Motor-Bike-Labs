56bfea2a41d42a326e09f4d5438c8e05ae5f291a
koiclone <cheryljcruz1@gmail.com>
2022-07-08 13:55:02 -0500

    changing class to className

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -8,28 +8,28 @@ function Header() {
     <>
       {/*header and navbar*/}
       <header>
-        <nav class='navbar' role='navigation' aria-label='main navigation'>
-          <div class='navbar-brand'>
-            <a class='navbar-item logo' href='/'>
-              <h1 class='title'>Motor Bike Labs</h1>
+        <nav className='navbar' role='navigation' aria-label='main navigation'>
+          <div className='navbar-brand'>
+            <a className='navbar-item logo' href='/'>
+              <h1 className='title'>Motor Bike Labs</h1>
             </a>
           </div>
 
-          <div class='navbar-end'>
-            <a class='navbar-item' href='#Home'>
+          <div className='navbar-end'>
+            <a className='navbar-item' href='#Home'>
               Home
             </a>
-            <a class='navbar-item' href='#Reviews'>
+            <a className='navbar-item' href='#Reviews'>
               Reviews
             </a>
-            <a class='navbar-item' href='#Contact'>
+            <a className='navbar-item' href='#Contact'>
               Contact
             </a>
-            <a class='navbar-item' href='#Account'>
+            <a className='navbar-item' href='#Account'>
               Account
             </a>
 
-            <a class='navbar-item' href='#Logout' onClick={Auth.logout}>
+            <a className='navbar-item' href='#Logout' onClick={Auth.logout}>
               Logout
             </a>
           </div>
@@ -37,34 +37,34 @@ function Header() {
       </header>
 
       {/* hero section, ****add carousel****/}
-      <section class='hero is-medium my-6'>
-        <div class='hero-body'>
-          <div class='title'>Exhaust your options</div>
+      <section className='hero is-medium my-6'>
+        <div className='hero-body'>
+          <div className='title'>Exhaust your options</div>
         </div>
       </section>
 
       {/*Tiles for motorcycle selection - section for image and section for stats*/}
-      <section class='tile is-ancestor'>
-        <div class='tile is-parent'>
-          <div class='tile is-child box'>
+      <section className='tile is-ancestor'>
+        <div className='tile is-parent'>
+          <div className='tile is-child box'>
             {/* image for motorcycle */}
-            <div class tile is-child container>
-              <figure class='image is-128x128'>
+            <div className tile is-child container>
+              <figure className='image is-128x128'>
                 <img
                   src='https://bulma.io/images/placeholders/128x128.png'
                   alt='Motorcycle'
                 />
               </figure>
-            <div class= 'container'>
-              
+            <div className= 'container stats'>
+
             </div>
             </div>
           </div>
         </div>
       </section>
 
-      <footer class='footer'>
-        <div class='content has-text-centered is-small'>
+      <footer className='footer'>
+        <div className='content has-text-centered is-small'>
           <p>
             &copy; 2022 | Built with &#128420; by Jugurta Maouchi, Jennifer
             Jennings, and Cheryl Cruz
