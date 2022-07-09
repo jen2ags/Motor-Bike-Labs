a36a868a40ae84d16812d4d0249a806e60b64ff8
koiclone <cheryljcruz1@gmail.com>
2022-07-08 21:39:50 -0500

    leveling stats tile

--- client/src/pages/SinglePage.js
+++ client/src/pages/SinglePage.js
@@ -6,7 +6,7 @@ function SinglePage() {
     <section className='tile is-ancestor is-flex-wrap-wrap'>
       <div className='tile is-parent is-vertical'>
         <div className='tile is-child'>
-            {/* main image */}
+          {/* main image */}
           <div className='card'>
             <div className='card-image  has-text-centered ' id='image'>
               <figure className='image is-inline-block  px-6 py-6'>
@@ -18,8 +18,24 @@ function SinglePage() {
             </div>
           </div>
         </div>
-         {/* stats */}
-        <div className= 'tile is-child'>
+      </div>
+      {/* stats */}
+      <div className='tile is-parent is-vertical has-text-centered'>
+        <div class=' tile is-child level px-6 mx-6 my-6'>
+          <div className='subtitle level-item' id='year'>
+            year
+          </div>
+          <div className='subtitle level-item' id='make'>
+            make
+          </div>
+          <div className='subtitle level-item' id='model'>
+            model
+          </div>
+        </div>
+        <div className='tile is-child box'>
+          <div className='' id='price'>
+            price
+          </div>
         </div>
       </div>
     </section>
