c8ccf06e474c5fcedbc05c26e929a414f76787eb
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-07-09 15:15:20 -0500

    got data back from a singleMotorcycle mutation, need only to display it

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/single" element={<SinglePage />}/>
+              <Route path="/single/:id" element={<SinglePage />}/>
             </Routes>
           </div>
       </Router>
