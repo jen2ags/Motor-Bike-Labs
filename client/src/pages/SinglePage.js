64cc7e1b7093eee9c0b653d3f9cdc64d7db6172c
koiclone <cheryljcruz1@gmail.com>
2022-07-08 21:24:45 -0500

    image tile for single page

--- client/src/pages/SinglePage.js
+++ client/src/pages/SinglePage.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import './singlePage.css'
-
+import React from 'react';
+import './singlePage.css';
+// columns
 function SinglePage() {
   return (
-    <div>SinglePage</div>
-  )
+    <section className='tile is-ancestor is-flex-wrap-wrap'>
+      <div className='tile is-parent is-vertical'>
+        <div className='tile is-child'>
+            {/* main image */}
+          <div className='card'>
+            <div className='card-image  has-text-centered ' id='image'>
+              <figure className='image is-inline-block  px-6 py-6'>
+                <img
+                  src='https://bulma.io/images/placeholders/480x800.png'
+                  alt='Motorcycle'
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+         {/* stats */}
+        <div className= 'tile is-child'>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default SinglePage
+export default SinglePage;
